cb2bc8f712cb9fb16c61669e5ecdec3c6ebe8a03
Danrus1100 <danrus228zk@gmail.com>
2025-08-16 22:23:35 +0300

    feat: fully functional "skin" tab

--- src/main/java/com/danrus/pas/render/gui/ButtonWithIcon.java
+++ src/main/java/com/danrus/pas/render/gui/ButtonWithIcon.java
@@ -1,23 +1,37 @@
 package com.danrus.pas.render.gui;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.AbstractButton;
 import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.components.WidgetSprites;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.ARGB;
+import net.minecraft.util.Mth;
 
 public class ButtonWithIcon extends Button {
 
+    private static final WidgetSprites SPRITES = new WidgetSprites(ResourceLocation.withDefaultNamespace("widget/button"), ResourceLocation.withDefaultNamespace("widget/button_disabled"), ResourceLocation.withDefaultNamespace("widget/button_highlighted"));
+
     public ResourceLocation icon;
 
-    protected ButtonWithIcon(int x, int y, int width, int height, ResourceLocation icon, Component message, OnPress onPress, CreateNarration createNarration) {
-        super(x, y, width, height, message, onPress, createNarration);
+
+    protected ButtonWithIcon(int x, int y, int width, int height, ResourceLocation icon, Component message, OnPress onPress) {
+        super(x, y, width, height, message, onPress, DEFAULT_NARRATION);
         this.icon = icon;
     }
 
     @Override
     protected void renderWidget(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick) {
-        super.renderWidget(guiGraphics, mouseX, mouseY, partialTick);
-        guiGraphics.blitSprite(RenderType::guiTextured, this.icon, this.getX(), this.getY(), 20, 20);
+        Minecraft minecraft = Minecraft.getInstance();
+        guiGraphics.blitSprite(RenderType::guiTextured, SPRITES.get(this.active, this.isHoveredOrFocused()), this.getX(), this.getY(), this.getWidth(), this.getHeight(), ARGB.white(this.alpha));
+        int i = this.active ? 16777215 : 10526880;
+////        this.renderString(guiGraphics, minecraft.font, i | Mth.ceil(this.alpha * 255.0F) << 24);
+//        int iStr = this.getX() + width;
+//        int jStr = this.getX() + this.getWidth() - width;
+        AbstractButton.renderScrollingString(guiGraphics, minecraft.font, this.getMessage(), this.getX() + 20, this.getY(), this.getX() + this.getWidth()-2, this.getY() + this.getHeight(), i | Mth.ceil(this.alpha * 255.0F) << 24);
+        guiGraphics.blitSprite(RenderType::guiTextured, this.icon, this.getX() + 1, this.getY() + 1, 18, 18);
     }
 }
--- src/main/java/com/danrus/pas/render/gui/ConfiguratorScreen.java
+++ src/main/java/com/danrus/pas/render/gui/ConfiguratorScreen.java
@@ -1,16 +1,20 @@
 package com.danrus.pas.render.gui;
 
+import com.danrus.pas.mixin.accessors.AnvilScreenAccessor;
 import com.danrus.pas.render.gui.tabs.Tab;
 import com.danrus.pas.render.gui.tabs.TabButton;
 import com.danrus.pas.render.gui.tabs.TabManager;
+import com.danrus.pas.utils.StringUtils;
 import com.danrus.pas.utils.VersioningUtils;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.components.EditBox;
 import net.minecraft.client.gui.components.ImageButton;
 import net.minecraft.client.gui.components.WidgetSprites;
 import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.gui.screens.inventory.AnvilScreen;
 import net.minecraft.client.gui.screens.inventory.InventoryScreen;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.core.Rotations;
@@ -21,12 +25,21 @@ import net.minecraft.world.entity.decoration.ArmorStand;
 import org.joml.Quaternionf;
 import org.joml.Vector3f;
 
+import java.util.List;
+
 public class ConfiguratorScreen extends Screen {
 
     public static final ResourceLocation ROTATE_BUTTON_TEXTURE = VersioningUtils.getResourceLocation("pas", "rotate_button");
     public static final ResourceLocation ROTATE_BUTTON_DISABLED_TEXTURE = VersioningUtils.getResourceLocation("pas", "rotate_button_disabled");
     public static final ResourceLocation ROTATE_BUTTON_HIGHLIGHTED_TEXTURE = VersioningUtils.getResourceLocation("pas", "rotate_button_highlighted");
     public static final ResourceLocation BACKGROUND_TEXTURE = VersioningUtils.getResourceLocation("pas", "pas_gui");
+
+    public static final ResourceLocation MOJANG_LOGO = VersioningUtils.getResourceLocation("pas", "mojang");
+    public static final ResourceLocation NAMEMC_LOGO = VersioningUtils.getResourceLocation("pas", "namemc");
+
+    public static final ResourceLocation WIDE_ARM_LOGO = VersioningUtils.getResourceLocation("pas", "wide");
+    public static final ResourceLocation SLIM_ARM_LOGO = VersioningUtils.getResourceLocation("pas", "slim");
+
     private static final float ANIMATION_SPEED = 0.15f;
 
     private float currentRotation = 0f;
@@ -46,16 +59,31 @@ public class ConfiguratorScreen extends Screen {
     private final TabButton overlayTabButton;
 
     private final ImageButton rotateButton;
+    private final Button acceptButton;
     private final ArmorStand entity;
-    private final Screen parent;
+    private final AnvilScreen parent;
+
+    private final ButtonWithIcon skinProviderButton;
+    private final ButtonWithIcon armTypeButton;
+
+    private String entityName = "";
+    private boolean isSlim = false;
+    private String skinProvider = "M";
+
+    private boolean hasCape = false;
+    private String capeProvider = "M";
+    private String capeId = "";
+
+    private boolean hasOverlay = false;
+    private String overlayName = "";
+    private int overlayBlend = 100;
 
     private final TabManager tabManager;
 
-    public ConfiguratorScreen(Screen parent) {
+    public ConfiguratorScreen(AnvilScreen parent) {
         super(Component.literal("Player Armor Stand Configurator"));
         this.parent = parent;
         this.entity = new ArmorStand(Minecraft.getInstance().level, 0, 0, 0);
-//        this.entity.setNoBasePlate(true);
 
         this.rotateButton = new ImageButton(0, 0, 20, 20,
                 new WidgetSprites(
@@ -65,36 +93,110 @@ public class ConfiguratorScreen extends Screen {
                 ),
                 button -> currentAnimationState = currentAnimationState == AnimationState.IDLE ? AnimationState.CAPE : AnimationState.IDLE
         );
+        this.acceptButton = Button.builder(Component.translatable("pas.menu.accept"), b -> acceptName()).bounds(width, height/2 + 90, 200, 20).build();
 
         skinTabButton = new TabButton(5, 5, 80, 15, Component.translatable("pas.menu.tab.skin"));
-        capeTabButton = new TabButton(105, 5, 80, 15, Component.literal("pas.menu.tab.cape"));
-        overlayTabButton = new TabButton(205, 5, 80, 15, Component.literal("pas.menu.tab.overlay"));
+        capeTabButton = new TabButton(105, 5, 80, 15, Component.translatable("pas.menu.tab.cape"));
+        overlayTabButton = new TabButton(205, 5, 80, 15, Component.translatable("pas.menu.tab.overlay"));
+
+        setupParams();
+
+        skinProviderButton = new ButtonWithIcon(0, 0, 120, 20,
+                MOJANG_LOGO, Component.translatable("pas.menu.tab.skin.provider." + skinProvider.toLowerCase()),
+                button -> {
+                    if (skinProvider.equals("M")) {
+                        skinProvider = "N";
+                        button.setMessage(Component.translatable("pas.menu.tab.skin.provider.n"));
+                    } else if (skinProvider.equals("N")) {
+                        skinProvider = "M";
+                        button.setMessage(Component.translatable("pas.menu.tab.skin.provider.m"));
+                    }
+                });
+
+        armTypeButton = new ButtonWithIcon(0, 0, 120, 20,
+                isSlim ? SLIM_ARM_LOGO : WIDE_ARM_LOGO,
+                Component.translatable("pas.menu.tab.skin.arm_type." + (isSlim ? "slim" : "wide")),
+                button -> {
+                    isSlim = !isSlim;
+                    ((ButtonWithIcon) button).icon = isSlim ? SLIM_ARM_LOGO : WIDE_ARM_LOGO;
+                    button.setMessage(Component.translatable("pas.menu.tab.skin.arm_type." + (isSlim ? "slim" : "wide")));
+                    setEntityName(entityName);
+                }
+                );
 
         this.tabManager = new TabManager(this);
+
         setupTabs();
+    }
+
+    private void setupParams () {
+
+        String itemName = ((AnvilScreenAccessor) parent).pas$getNameInput().getValue();
+
+        if (!itemName.equals(Component.translatable("item.minecraft.armor_stand").getString())) {
+            List<String> matches = StringUtils.matchASName(((AnvilScreenAccessor) parent).pas$getNameInput().getValue());
+            entityName = matches.get(0);
+
+            // FIXME: toooo much if-else statements next:
+
+            if (matches.get(1).contains("S")) {
+                isSlim = true;
+            }
+            if (matches.get(1).contains("C")) {
+                hasCape = true;
+            }
+            if (matches.get(1).contains("T")) {
+                hasOverlay = true;
+                overlayName = matches.get(2);
+                overlayBlend = Integer.parseInt(matches.get(3));
+            }
+            if (matches.get(1).contains("N")) {
+                skinProvider = "N";
+            }
+
+            setEntityName(itemName);
+
+        }
     }
 
     private void setupTabs() {
         // --- Skin Tab ---
         EditBox nameBox = new EditBox(Minecraft.getInstance().font, 0, 0, 100, 20, Component.literal("Name"));
-        TextWidget nameLabel = new TextWidget(0, 0, 100, 20, Component.literal("test"));
+        nameBox.setValue(entityName);
+        TextWidget nameLabel = new TextWidget(0, 0, 100, 20, Component.translatable("pas.menu.tab.skin.name"));
         ImageButton acceptNameButton = new ImageButton(0, 0, 20, 20,
                 new WidgetSprites(
                         VersioningUtils.getResourceLocation("pas", "accept"),
                         VersioningUtils.getResourceLocation("pas", "accept_disabled"),
                         VersioningUtils.getResourceLocation("pas", "accept_highlighted")
                 ),
-                button -> setEntityName(nameBox.getValue())
+                button -> {
+                    entityName = nameBox.getValue();
+                    setEntityName(entityName);
+                }
         );
-
+        TextWidget skinProviderLabel = new TextWidget(0, 0, 100, 20, Component.translatable("pas.menu.tab.skin.provider"));
+        
+        
+        TextWidget armTypeLabel = new TextWidget(0, 0, 100, 20, Component.translatable("pas.menu.tab.skin.arm_type"));
+        
         Tab skinTab = new Tab("skin", (width, height) -> {
-            nameLabel.setPosition(Math.round(width / 2f ), Math.round(height / 2f - 87));
+            nameLabel.setPosition(Math.round(width / 2f + 2), Math.round(height / 2f - 87));
             nameBox.setPosition(Math.round(width / 2f - 8), Math.round(height / 2f - 70));
             acceptNameButton.setPosition(Math.round(width / 2f + 92), Math.round(height / 2f - 70));
+            skinProviderLabel.setPosition(Math.round(width / 2f + 2), Math.round(height / 2f - 50));
+            skinProviderButton.setPosition(Math.round(width / 2f - 8), Math.round(height / 2f - 30));
+            armTypeLabel.setPosition(Math.round(width / 2f + 2), Math.round(height / 2f - 10));
+            armTypeButton.setPosition(Math.round(width / 2f - 8), Math.round(height / 2f + 10));
         });
+
         skinTab.addWidget(nameBox);
         skinTab.addWidget(nameLabel);
         skinTab.addWidget(acceptNameButton);
+        skinTab.addWidget(skinProviderLabel);
+        skinTab.addWidget(skinProviderButton);
+        skinTab.addWidget(armTypeLabel);
+        skinTab.addWidget(armTypeButton);
 
         // -- Cape Tab ---
 
@@ -117,6 +219,7 @@ public class ConfiguratorScreen extends Screen {
     protected void init() {
 //        this.addRenderableWidget(rotateButton); // Removed =(
         tabManager.init();
+        this.addRenderableWidget(acceptButton);
         repositionElements(this.width, this.height);
     }
 
@@ -124,6 +227,8 @@ public class ConfiguratorScreen extends Screen {
         this.addRenderableWidget(skinTabButton);
         this.addRenderableWidget(capeTabButton);
         this.addRenderableWidget(overlayTabButton);
+
+        this.acceptButton.setPosition(width/2 - 100, height/2 + 120);
 
         this.rotateButton.setPosition(Math.round(width / 2f - 38), Math.round(height / 2f + 79));
         this.skinTabButton.setPosition(width/2 - 124, height/2 - 109);
@@ -135,7 +240,6 @@ public class ConfiguratorScreen extends Screen {
     @Override
     public void resize(Minecraft minecraft, int width, int height) {
         super.resize(minecraft, width, height);
-
         repositionElements(width, height);
     }
 
@@ -179,7 +283,13 @@ public class ConfiguratorScreen extends Screen {
             }
         }
 
-        g.drawCenteredString(Minecraft.getInstance().font, "Настройка скина", this.width / 2, 15, 0xFFFFFF);
+        if (skinProvider.equals("M")) {
+            skinProviderButton.icon = MOJANG_LOGO;
+        } else if (skinProvider.equals("N")) {
+            skinProviderButton.icon = NAMEMC_LOGO;
+        }
+
+        g.drawCenteredString(Minecraft.getInstance().font, Component.translatable("pas.menu.name"), this.width / 2, 15, 0xFFFFFF);
         entity.setHeadPose(new Rotations(currentHeadX, currentHeadY, currentHeadZ));
 
         Quaternionf rotation = new Quaternionf().rotateX((float) Math.PI * 1.1F)
@@ -205,7 +315,7 @@ public class ConfiguratorScreen extends Screen {
 
     private void setEntityName(String name) {
         if (name != null && !name.isEmpty()) {
-            entity.setCustomName(Component.literal(name));
+            entity.setCustomName(Component.literal(generateEntityName()));
         } else {
             entity.setCustomName(null);
         }
@@ -227,6 +337,20 @@ public class ConfiguratorScreen extends Screen {
                 targetHeadZ = 0f;
             }
         }
+    }
+
+    private String generateEntityName() {
+        return entityName + "|" +
+                (isSlim ? "S" : "") +
+                (hasCape ? "C" : "") +
+                (skinProvider.equals("N") ? "N" : "") +
+                (hasOverlay ? "T" + overlayName + "%" + overlayBlend : "");
+    }
+
+    private void acceptName() {
+        Minecraft.getInstance().setScreen(parent);
+        String toAnvil = generateEntityName();
+        ((AnvilScreenAccessor) parent).pas$getNameInput().setValue(toAnvil);
     }
 
     private enum AnimationState {
