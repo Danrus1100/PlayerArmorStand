e921ffcaa00f071d0301934b75e2bb6acc3d0d49
Danrus1100 <danrus228zk@gmail.com>
2025-08-28 19:21:10 +0300

    chore: additional command name

--- src/main/java/com/danrus/pas/utils/managers/CommandsManager.java
+++ src/main/java/com/danrus/pas/utils/managers/CommandsManager.java
@@ -7,23 +7,25 @@ import com.mojang.brigadier.context.CommandContext;
 import dev.architectury.event.events.client.ClientCommandRegistrationEvent;
 import net.minecraft.network.chat.Component;
 
+import java.util.List;
+
 public class CommandsManager {
+
+    private static final List<String> COMMANDS_NAMES = List.of("player-armor-stands", "pas");
+
     public static void init() {
-        ClientCommandRegistrationEvent.EVENT.register(((dispatcher, context) ->
-                dispatcher.register(ClientCommandRegistrationEvent.literal("player-armor-stands").executes(CommandsManager::defaultCommand)
-                .then(ClientCommandRegistrationEvent.literal("reload_failed").executes(CommandsManager::reloadFailedCommand))
-                .then(ClientCommandRegistrationEvent.literal("reload").then(ClientCommandRegistrationEvent.argument("name/skin", new SkinDataArgument()).executes(CommandsManager::reloadSingeCommand)))
-                )
-        ));
+        COMMANDS_NAMES.forEach((command_name) -> {
+            ClientCommandRegistrationEvent.EVENT.register(((dispatcher, context) ->
+                    dispatcher.register(ClientCommandRegistrationEvent.literal(command_name).executes(CommandsManager::defaultCommand)
+                            .then(ClientCommandRegistrationEvent.literal("reload_failed").executes(CommandsManager::reloadFailedCommand))
+                            .then(ClientCommandRegistrationEvent.literal("reload").then(ClientCommandRegistrationEvent.argument("name/skin", new SkinDataArgument()).executes(CommandsManager::reloadSingeCommand)))
+                    )
+            ));
+        });
     }
 
     private static int reloadSingeCommand(CommandContext<ClientCommandRegistrationEvent.ClientCommandSourceStack> context) {
         SkinData data = SkinDataArgument.getData(context, "name/skin");
-//        if (name.isEmpty()) {
-//            context.getSource().arch$sendFailure(Component.translatable("commands.pas.reload_failed_error"));
-//            return 0;
-//        }
-//        SkinManger.getInstance().reloadData(name);
         data.setStatus(DownloadStatus.IN_PROGRESS);
         String name = data.getParams().isEmpty() ? data.getName() : data.getName() + "|" + data.getParams();
         SkinManger.getInstance().reloadData(name);
